a88e69ec400337bbdb8c07615f41c5f73cce9b3c
Shakira Syevuo <shakirasyevuo@gmail.com>
2025-03-24 21:24:46 +0300

    Completed code along

--- server/app.py
+++ server/app.py
@@ -2,4 +2,18 @@
 
 from flask import Flask
 
+# helps file figure out where the application is and where its important files will be
 app = Flask(__name__)
+
+# defining routes
+@app.route('/')
+def index():
+    return '<h1>Welcome to my page!</h1>'
+
+@app.route('/<string:username>')
+def user(username):
+    return f'<h1>Profile for {username}</h1>'
+
+# development server
+if __name__ == '__main__':
+    app.run(port=5555, debug=True)
